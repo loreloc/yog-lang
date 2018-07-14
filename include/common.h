d912a3eecbde4ed57779ddd8e566f0666689bb8d
loreloc <lorenzoloconte@outlook.it>
2018-07-14 18:51:35 +0200

    added custom calloc, realloc and strdup functions

--- include/common.h
+++ include/common.h
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * @brief Custom succeed-or-die memory allocation function
@@ -14,8 +15,31 @@
 void *ymalloc(size_t bytes);
 
 /**
+ * @brief Custom succeed-or-die zero initialized memory allocation function
+ * @param num The number of blocks to allocate
+ * @param size The size of each block
+ * @return A pointer to the allocated memory block of size at least 1 byte
+ */
+void *ycalloc(size_t num, size_t size);
+
+/**
+ * @brief Custom succeed-or-die memory reallocation function
+ * @param ptr A pointer to the memory block to reallocate
+ * @param bytes The number of bytes to reallocate
+ * @return A pointer to the reallocated memory block of size at least 1 byte
+ */
+void *yrealloc(void *ptr, size_t size);
+
+/**
  * @brief Custom memory free function
  * @param ptr A pointer to the memory block to free
  */
 void yfree(void *ptr);
+
+/**
+ * @brief Custom succeed-or-die string duplicate function
+ * @param str A pointer to the string to duplicate
+ * @return A pointer to a new string
+ */
+char *ystrdup(char *str);
 
--- src/common.c
+++ src/common.c
@@ -16,9 +16,48 @@ void *ymalloc(size_t bytes)
 	return ptr;
 }
 
+void *ycalloc(size_t num, size_t size)
+{
+	if(size == 0)
+		size = 1;
+	
+	void *ptr = calloc(num, size);
+	if(ptr == NULL)
+	{
+		fputs("memory allocation failed\n", stderr);
+		exit(~0x0);
+	}
+
+	return ptr;
+}
+
+void *yrealloc(void *ptr, size_t size)
+{
+	if(size == 0)
+		size = 1;
+
+	ptr = realloc(ptr, size);
+	if(ptr == NULL)
+	{
+		fputs("memory reallocation failed\n", stderr);
+		exit(~0x0);
+	}
+
+	return ptr;
+}
+
 void yfree(void *ptr)
 {
 	if(ptr != NULL)
 		free(ptr);
+}
+
+char *ystrdup(char *str)
+{
+	char *ptr = ymalloc(strlen(str) + 1);
+
+	strcpy(ptr, str);
+
+	return ptr;
 }
 
