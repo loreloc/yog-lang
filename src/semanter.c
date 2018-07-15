403810c6d87d4a3d6b2764a0d04c1f14d5b21a0d
loreloc <lorenzoloconte@outlook.it>
2018-07-15 13:46:32 +0200

    added abstract syntax tree to expression tree translation

--- src/semanter.c
+++ src/semanter.c
@@ -73,7 +73,55 @@ struct instr_list analyse_statements(struct semantic_context *ctx)
 
 struct expr_tree *translate_ast_expr_tree(struct ast *syn_tree)
 {
-	// TODO:
-	return expr_tree_make_literal(0);
+	struct expr_tree *tree;
+
+	switch(syn_tree->type)
+	{
+		case AST_NODE_EXPRESSION:
+			if(syn_tree->subtrees[1] != NULL)
+			{
+				tree = expr_tree_make_operator((syn_tree->subtrees[1]->type == AST_NODE_PLUS) ? OP_PLUS : OP_MINUS);
+				tree->left = translate_ast_expr_tree(syn_tree->subtrees[0]);
+				tree->right = translate_ast_expr_tree(syn_tree->subtrees[2]);
+			}
+			else
+				tree = translate_ast_expr_tree(syn_tree->subtrees[0]);
+			break;
+
+		case AST_NODE_TERM:
+			if(syn_tree->subtrees[1] != NULL)
+			{
+				tree = expr_tree_make_operator((syn_tree->subtrees[1]->type == AST_NODE_MUL) ? OP_MUL : OP_DIV);
+				tree->left = translate_ast_expr_tree(syn_tree->subtrees[0]);
+				tree->right = translate_ast_expr_tree(syn_tree->subtrees[2]);
+			}
+			else
+				tree = translate_ast_expr_tree(syn_tree->subtrees[0]);
+			break;
+
+		case AST_NODE_FACTOR:
+			tree = translate_ast_expr_tree(syn_tree->subtrees[0]);
+			break;
+
+		case AST_NODE_PLUS:
+			tree = expr_tree_make_operator(OP_PLUS);
+			tree->right = translate_ast_expr_tree(syn_tree->subtrees[0]);
+			break;
+
+		case AST_NODE_MINUS:
+			tree = expr_tree_make_operator(OP_MINUS);
+			tree->right = translate_ast_expr_tree(syn_tree->subtrees[0]);
+			break;
+
+		case AST_NODE_LITERAL:
+			tree = expr_tree_make_literal(syn_tree->value.lit);
+			break;
+
+		default: // case AST_NODE_IDENTIFIER:
+			tree = expr_tree_make_symbol(syn_tree->value.sym);
+			break;
+	}
+
+	return tree;
 }
 
