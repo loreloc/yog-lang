16327bb5ccc660dc4e47613c258b83bc155aa499
loreloc <lorenzoloconte@outlook.it>
2018-07-16 12:53:46 +0200

    simplified assign instruction rappresentation

--- src/parser.c
+++ src/parser.c
@@ -124,7 +124,6 @@ struct ast *parse_statements(struct parse_context *ctx)
 			{
 				instr_tree = ast_make(AST_NODE_ASSIGN);
 				ast_add_subtree_symbol(instr_tree, sym);
-				ast_add_subtree_node(instr_tree, AST_NODE_EQUAL);
 				ast_add_subtree(instr_tree, parse_expression(ctx));
 			}
 		}
